0f8d1d4c3fdc8c3aa573e8257eee46821f44879b
sarafrozbekmurodova <sarafroz1997@mail.ru>
2020-12-22 02:22:18 +0500

    router_comment_deleted

--- router.js
+++ router.js
@@ -4,45 +4,6 @@ const Task = require('./models/Task')
 const Post = require('./models/Post');
 const Author = require('./models/Author');
 
-// // List of tasks
-// router.get('/', (req, res) => {
-//     Task.find({}).exec((err, tasks) => {
-//         res.send(tasks)
-//     })
-// });
-
-// // Create
-// router.get('/create', (req, res) => {
-//     res.render('create')
-// })
-
-// router.post('/create', (req, res) => {
-//     var task = new Task(req.body);
-//     task.save().then(item => {
-//     	res.send(item)
-//     })
-// })
-
-// // Single task
-// router.get('/:id', (req, res) => {
-//     Task.find({ _id: req.params.id }).exec((err, task) => {
-//         res.send(task)
-//     })
-// })
-
-// router.post('/update/:id', (req, res) => {
-//     Task.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, (err, task) =>{
-//         res.send(task)
-//     })
-// })
-
-// router.get('/delete/:id', (req, res) => {
-//     Task.findOneAndDelete({ _id: req.params.id }).exec((err, task) => {
-//         res.send('Task deleted');
-//     })
-// })
-
-
 
 // List of posts
 router.get('/', (req, res) => {
@@ -72,39 +33,14 @@ router.post('/create_post', (req, res) => {
     })
 })
 
-// //List Table data
-// router.get('/posts',(req,res)=>{
-//     Post.find(function(err,posts) {
-//         if (err) {
-//             console.log(err);
-//         }else{
-            
-//             res.render('read_post',{posts:posts})
-          
-       
-//         }
-//     })
-// })
-
 
 // get Single post by id 
 router.get('/posts/:id', (req, res) => {
    Post.findById( req.params.id).exec((err,data)=>{
 res.render('postid',data)
-
- 
    })
-        //res.render('/', req.params.id) 
+        
 })
-
-
-
-// // Single post
-// router.get('/:id', (req, res) => {
-//     Post.find({ _id: req.params.id }).exec((err, posts) => {
-//         res.render('display',posts)
-//     })
-// })
 
 
 //edit single post
@@ -123,7 +59,7 @@ router.post('/edit_post/:id', (req, res) => {
         body:req.body.body,
         author_id:req.body.author_id
     }
-    Post.findOneAndUpdate(req.params.id, /*$set: req.body */postbyid, (err) =>{
+    Post.findOneAndUpdate(req.params.id, postbyid, (err) =>{
         res.redirect('/')
     })
 })
